b3aca1eaa5b6f5d3c5ae627b855e9b2d5f03c90f
Maxime <maxmtlfr@gmail.com>
2023-04-07 13:39:14 +0200

    logique react
    TODO
    ADD items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Checkout } from "./components/Checkout/Checkout";
+import "./App.scss";
+const items = [
+	{
+	id: 1,
+	name: "T-Shirt",
+	price: 19.99,
+	image: "src/assets/img/photo1.png"
+	},
+	{
+	id: 2,
+	name: "Jeans",
+	price: 39.99,
+	image: "src/assets/img/photo2.png"
+	}
+	];
 function App() {
-
+ return (
+  <>
+   <h1>Checkout</h1>
+	<Checkout items={items} />  </>
+ );
 }
 
-export default App
+export default App;
