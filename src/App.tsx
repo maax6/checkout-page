2f28401087d00988a8647fb02d3ff9eebe24bdea
Maxime <85428733+maax6@users.noreply.github.com>
2023-05-04 17:32:48 +0200

    Add functions to increment shipping and items + flex Life

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import { Checkout } from './components/Checkout/Checkout';
 import './App.scss';
+
 const items = [
   {
     id: 1,
-    name: 'T-Shirt',
-    price: 19.99,
+    name: 'Vintage Backbag',
+    price: 54.99,
+    oldPrice: 94.99,
+    quantity: 1,
     image: 'src/assets/img/photo1.png',
   },
   {
     id: 2,
-    name: 'Jeans',
-    price: 39.99,
+    name: 'Levi Shoes',
+    price: 74.99,
+    oldPrice: 124.99,
+    quantity: 1,
     image: 'src/assets/img/photo2.png',
   },
 ];
+
 function App() {
   return (
     <>
-      <Checkout items={items} />{' '}
+      <Checkout items={items} />
     </>
   );
 }
--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,10 +1,13 @@
 import './_checkout.scss';
+import { useState } from 'react';
 
 interface Item {
   id: number;
   name: string;
   price: number;
+  oldPrice: number;
   image: string;
+  quantity: number;
 }
 
 interface CheckoutProps {
@@ -12,6 +15,32 @@ interface CheckoutProps {
 }
 
 export function Checkout({ items }: CheckoutProps) {
+  const [cartItems, setCartItems] = useState<Item[]>(items || []);
+  const calculateShippingCost = () => {
+	const baseCost = 5;
+	const additionalCostPerThreeItems = 2;
+	const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+	const additionalCost = Math.floor(totalItems / 3) * additionalCostPerThreeItems;
+	return baseCost + additionalCost;
+  };
+  const [shippingCost, setShippingCost] = useState<number>(calculateShippingCost());
+
+  const calculateSubtotal = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+  const updateQuantity = (itemId: number, change: number) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === itemId
+          ? { ...item, quantity: Math.max(1, item.quantity + change) }
+          : item
+      )
+    );
+	setShippingCost(calculateShippingCost());
+  };
   return (
     <div className="page-wrapper">
       <h1>Checkout</h1>
@@ -45,10 +74,9 @@ export function Checkout({ items }: CheckoutProps) {
           <button className="checkout__form__submit">Continue</button>
         </form>
         <div className="checkout__cart">
-          <h2 className="checkout__cart__title">Shopping Cart</h2>
-          {items && Array.isArray(items) && items.length > 0 ? (
+          {cartItems && Array.isArray(cartItems) && cartItems.length > 0 ? (
             <ul className="checkout__cart__items">
-              {items.map((item) => (
+              {cartItems.map((item) => (
                 <li className="checkout__cart__item" key={item.id}>
                   <img
                     className="checkout__cart__item__image"
@@ -59,14 +87,29 @@ export function Checkout({ items }: CheckoutProps) {
                     <span className="checkout__cart__item__name">
                       {item.name}
                     </span>
-                    <span className="checkout__cart__item__price">
-                      <span className="checkout__cart__item__price__old">
+                    <div className="checkout__cart__item__price">
+                      <span className="checkout__cart__item__price__new">
                         ${item.price.toFixed(2)}
                       </span>
-                      <span className="checkout__cart__item__price__new">
-                        ${(item.price * 0.8).toFixed(2)}
+                      <span className="checkout__cart__item__price__old">
+                        ${item.oldPrice.toFixed(2)}
                       </span>
-                    </span>
+                    </div>
+                    <div className="checkout__cart__item__quantity">
+					<button
+                        className="checkout__cart__item__quantity__button"
+                        onClick={() => updateQuantity(item.id, -1)}
+                      >
+                        -
+                      </button>
+                      <span className='checkout__cart__item__quantity__nmbr'>{item.quantity}</span>
+                      <button
+                        className="checkout__cart__item__quantity__button"
+                        onClick={() => updateQuantity(item.id, 1)}
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
                 </li>
               ))}
@@ -77,17 +120,17 @@ export function Checkout({ items }: CheckoutProps) {
           <div className="checkout__cart__summary">
             <div className="checkout__cart__summary__row">
               <span>Subtotal</span>
-              <span>40.00</span>
+              <span>${calculateSubtotal().toFixed(2)}</span>
             </div>
             <hr />
             <div className="checkout__cart__summary__row">
               <span>Shipping</span>
-              <span>5.00</span>
+              <span>${shippingCost.toFixed(2)}</span>
             </div>
             <hr />
             <div className="checkout__cart__summary__row checkout__cart__summary__row--total">
               <span>Total</span>
-              <span>45.00</span>
+              <span>${(calculateSubtotal() + shippingCost).toFixed(2)}</span>
             </div>
           </div>
         </div>
